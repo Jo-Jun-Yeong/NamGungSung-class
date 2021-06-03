39babcfb0dcd3bc351ce446f9fe7ce3161762f8e
jojun-yeong <jojun-yeong@localhost>
2021-06-03 16:10:32 +0900

    06/03

--- src/Nam_prac.java
+++ src/Nam_prac.java
@@ -324,25 +324,57 @@ public class Nam_prac {
 		
 //		-> code
 //		
-		int num = 12345;
-		int sum = 0;
-		
-		for(int i = 1; i <=(int)(Math.log10(num)+1); i++) {
-			num = ((num%(10^i))/10^(i-1));
-			System.out.println("num ["+i+"]: "+num);
-			sum += num;
-			System.out.println("sum : "+sum);
-		}
-		System.out.println("sum : "+sum);
+//		int num = 12345;
+//		int sum = 0;
+//		
+//		for(int i = 1; i <=(int)(Math.log10(num)+1); i++) {
+//			num = num%10^(i-1);
+//			System.out.println("num ["+i+"]: "+num);
+//			sum += num;
+//			System.out.println("sum : "+sum);
+//		}
+//		System.out.println("sum : "+sum);
 		
 		
 		
 		
 		
 //		----------------------------------------------------------------		
-//		Q
+//		Q[4-11] 피보나치(Fibonnaci) 수열(數列)은 앞을 두 수를 더해서 다음 수를 만들어 나가 는 수열이다.
+//		예를 들어 앞의 두 수가 1과 1이라면 
+//		그 다음 수는 2가 되고 그 다음 수는 1과 2를 더해서 3이 되어서 1,1,2,3,5,8,13,21,... 과 같은 식으로 진행된다. 
+//		1과 1부터 시작하는 피보나치수열의 10번째 수는 무엇인지 계산하는 프로그램을 완성하시오
+		
+//		-> code
+		// Fibonnaci 수열의 시작의 첫 두 숫자를 1, 1로 한다.
+		
+//		1,1,2,3,5,8,13,21,34, 55
+		//전+현 = 전
+//		int num1 = 1; 	// = i
+//		int num2 = 1; 	// = i
+//		
+//		
+//		int num3=num1+num2; 
+////		System.out.print(num1+","+num2);
+//
+//		for (int i = 0 ; i < 8 ; i++ ) {
+//			
+//		}
 //		----------------------------------------------------------------		
-//		Q
+//		Q4-12구구단의 일부분을 다음과 같이 출력하시오.
+		
+//		-> code
+		
+		for(int i = 2; i<10; i++) {
+			
+			for(int j= 1; j<4; j++ ) {
+				
+				System.out.println(i+"*"+j+"="+(i*j));
+				
+				
+			} 				//for2
+				System.out.println("");
+		} 					//for1
 //		----------------------------------------------------------------		
 //		Q
 //		----------------------------------------------------------------		
