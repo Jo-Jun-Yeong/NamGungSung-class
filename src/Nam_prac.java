efae21a9183a63909184d7b6c0012e309459eb57
test1 <jojun-yeong@localhost>
2021-06-05 00:42:58 +0900

    06/05

--- src/Nam_prac.java
+++ src/Nam_prac.java
@@ -364,19 +364,42 @@ public class Nam_prac {
 //		Q4-12구구단의 일부분을 다음과 같이 출력하시오.
 		
 //		-> code
-		
-		for(int i = 2; i<10; i++) {
-			
-			for(int j= 1; j<4; j++ ) {
-				
-				System.out.println(i+"*"+j+"="+(i*j));
-				
-				
-			} 				//for2
-				System.out.println("");
-		} 					//for1
+//		
+//		for(int i = 2; i<10; i++) {
+//			
+//			for(int j= 1; j<4; j++ ) {
+//				
+//				System.out.println(i+"*"+j+"="+(i*j));
+//				
+//				
+//			} 				//for2
+//				System.out.println("");
+//		} 					//for1
 //		----------------------------------------------------------------		
-//		Q
+//		Q4-13다음은 주어진 문자열(value)이 숫자인지를 판별하는 프로그램이다.
+//			(1)에 알맞 은 코드를 넣어서 프로그램을 완성하시오.
+		
+//		-> code
+		
+		String value = "12o34"; 
+		char ch = ' ';
+		boolean isNumber = true;
+		// 반복문과 charAt(int i)를 이용해서 문자열의 문자를 
+		// 하나씩 읽어서 검사한다.
+		
+		for(int i=0; i < value.length() ;i++) {
+		
+		if( !( (value.charAt(i)<57) && (value.charAt(i)>48) ) ) {
+			isNumber = false;
+			System.out.println(isNumber);
+		}
+		
+		} //for
+		if (isNumber) {			
+		System.out.println(value+"는 숫자입니다.");
+		} else {
+			System.out.println(value+"는 숫자가 아닙니다.");			
+		} 	//if and else
 //		----------------------------------------------------------------		
 //		Q
 		
