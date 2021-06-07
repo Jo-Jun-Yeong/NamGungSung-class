de6fd1444f606540213a757cc170ebf66b3a7430
test1 <jojun-yeong@localhost>
2021-06-07 15:07:15 +0900

    re:check 4

--- src/Nam_prac_4.java
+++ src/Nam_prac_4.java
@@ -1,33 +1,28 @@
 import java.util.*;
 
-public class Nam_prac {
+public class Nam_prac_4 {
 	public static void main(String [] args) {
 //		Q2. 사과를 담을 바구니의 갯수를 계산하는 코드
 		
 		//->
 		
-//		int NumOfApple = 123; 	//사과의 수
-//		int SizeOfBucket = 10;	//바구니의 크기(바구니에 담을수 있는 사과의 갯수)
-//		int NumOfBucket = NumOfApple / SizeOfBucket;	//모든 사과를 담는데 필요한 바구니의 수
-//		
-//		System.out.println("바구니의 수 : " + NumOfBucket);
+//		int numOfApples = 123; 	//사과의 수
+//		int sizeOfBucket = 10;	//바구니의 크기(바구니에 담을수 있는 사과의 갯수)
+////		int NumOfBucket = NumOfApple / SizeOfBucket;	//모든 사과를 담는데 필요한 바구니의 수
+//		int numOfBucket =
+//				numOfApples/sizeOfBucket + (numOfApples%sizeOfBucket > 0 ? 1 : 0) ;
+////		
+//		System.out.println("바구니의 수 : " + numOfBucket);
 //		----------------------------------------------------------------
 		
 //		Q3.아래는 변수 num의 값에 따라 ‘양수’, ‘음수’, ‘0’을 출력하는 코드이다.
 //		삼항 연산 자를 이용해서 (1)에 알맞은 코드를 넣으시오
 		
 		//->
-		
+//		
 //		int num = 10;
-//		if (num > 0) {
-//			System.out.println("양수");
-//		}
-//		else if (num < 0 ) {
-//			System.out.println("음수");
-//		}
-//		else System.out.println("0");
-//
-//		System.out.println(  );
+//		System.out.println(  (num > 0 ? "양수" :(num < 0 ? "음수" : "0"))  );
+
 		
 //		----------------------------------------------------------------		
 //		Q4. 아래는 변수 num의 값 중에서 백의 자리 이하를 버리는 코드이다. 
@@ -58,9 +53,10 @@ public class Nam_prac {
 		
 		//->
 		
-//		int num = 24;
-//		System.out.println( Math.abs(num-100 )% 10 );
-		
+//		int num = ;
+//		System.out.println( Math.abs(num-100 )% 10 );	// xx
+//		System.out.println(10 - num%10); 				// OO
+//		
 //		----------------------------------------------------------------		
 //		Q7.아래는 화씨(Fahrenheit)를 섭씨(Celcius)로 변환하는 코드이다. 
 //		변환공식이 'C = 5/9 ×(F - 32)'라고 할 때, (1)에 알맞은 코드를 넣으시오. 
@@ -68,11 +64,12 @@ public class Nam_prac {
 //		(Math.round()를 사용하지 않고 처리할 것)
 		
 		//->
-		
+//		
 //		int fahrenheit = 100;
-//
-//		float celcius = (float) Math.ceil((5.0 / 9.0) * (fahrenheit - 32)*100)/100;
-//		 
+////
+//		float celcius = /*(float) Math.ceil((5.0 / 9.0) * (fahrenheit - 32)*100)/100;*/
+//						(int)((5/9f * (fahrenheit - 32))*100 + 0.5) / 100f;
+////		 
 //		System.out.println("Fahrenheit:"+fahrenheit); 
 //		System.out.println("Celcius:"+celcius);
 
@@ -91,6 +88,9 @@ public class Nam_prac {
 //		long l = 3000 * 3000 * 3000L;
 //		float f2 = 0.1f; double d = 0.1;
 //		boolean result = d!=f2;
+//		->
+//		boolean resul = (float)d == f2;
+		// 형변환시 큰타입을 작은타입으로 변환하는방법이 더 정확한 값을 얻어낼 수 있다.
 //		System.out.println("c="+c);
 //		System.out.println("ch="+ch);
 //		System.out.println("f="+f);
@@ -121,7 +121,7 @@ public class Nam_prac {
 //		}
 //		else 
 //			System.out.println(false);
-//		
+		
 //		System.out.println(b);
 		
 //		----------------------------------------------------------------		
@@ -134,6 +134,7 @@ public class Nam_prac {
 //		->
 //		char ch = 'A';
 //		char d = (char) ((ch>='A' && ch<='Z') ? (ch + ' ') : ch);
+//		// ' '(공백)은 32값와 같다 따라서
 //		
 //		System.out.println("ch:"+ch); 
 //		System.out.println("ch to lowerCase:"+d);
@@ -141,38 +142,58 @@ public class Nam_prac {
 //		----------------------------------------------------------------		
 //		Q9.
 //		int x=9;
-////		1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
-//		if(10<=x && 20>=x) 
+////	1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
+////		->
+//		if
+////		(10<=x && 20>=x)
+//		(10<=x && x <= x) 	//better
 //			System.out.println("1. : " + true);
 //		
-////		2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
+////	2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
+////		->
 //		char ch2='1';
-//		if(ch2 !=' '&& ch2 !='	')
+//		if
+////		(ch2 !=' '&& ch2 !='	') or
+//		(!(ch2 == ' ' || ch2 == '\t'))
 //			System.out.println("2. : " + true);
 //
-////		3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
+////	3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
+////		->
 //		char ch3 = 'x';
-//		if(ch3=='x' && ch3 =='X') 
+//		if(ch3=='x' || ch3 =='X') 
 //			System.out.println("3. : "+true);
 //		
-////		4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
+////	4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
+////		->
 //		char ch4 = '8';
-//		if(ch4>='0' && '9'>=ch4) 
+//		if
+////		(ch4>='0' && '9'>=ch4) 
+//		('0' < ch4 && ch4 <'9') 	//better
 //			System.out.println("4. : "+true);
 //		
 ////		5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
+////		->
 //		char ch5 = 's';
 //		if((ch5>='a' && 'z'>= ch5)||(ch5>='A' && 'Z'>= ch5)) 
 //			System.out.println("5. : "+true);
+//		
 ////		6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지
 ////		않을 때 true인 조건식
+////		->
 //		int year = 400;
 //		if(year%400 == 0 ||(year % 4 ==0 && year % 4 != 0) )
 //			System.out.println("6. : "+ true);
+//		
 ////		7. boolean형 변수 powerOn가 false일 때 true인 조건식 
+////		->
 //		boolean powerOn = false;
-//		if(powerOn == false) System.out.println("7. : "+true);
+//		if
+////		(powerOn == false) or
+//		(powerOn == !powerOn)		
+//			System.out.println("7. : "+!powerOn);
+//		
 ////		8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
+////		->
 //		String str = "yes";
 //		if(str.equals("yes")) System.out.println("8. : "+true);
 		
@@ -184,10 +205,11 @@ public class Nam_prac {
 //		int sum=0;
 //		for(int i = 1; i<=20; i++) {
 //			
-//			if(!(i%2==0 && i%3==0)) {
+//			if ( ( !(i%2==0) && !(i%3==0) ) )	
+//			{
 //				sum += i;
 //			}
-//			System.out.println("sum : "+sum);
+//			System.out.println("sum["+ i + "] :" +sum);
 //		}
 //		----------------------------------------------------------------		
 //		Q4-3 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.	
@@ -195,12 +217,15 @@ public class Nam_prac {
 		
 //		->
 		
-//		int a = 1;
-//		for(int i = 1; i <=10; i++) {
-//			a= a+i;
+//		int a = 0;
+//		int sum = 0;
+//		for(int i = 1; i <11; i++) {
 //			
+//			a= a+i;
+//			sum += a;
 //
-//			System.out.println("sum["+(i)+"]"+(a-1));
+//			
+//			System.out.println("sum["+(i)+"]"+(sum));
 //		}//for1
 	
 //		----------------------------------------------------------------		
@@ -214,7 +239,23 @@ public class Nam_prac {
 //			sum = sum +(i+2);
 //			System.out.println("sum ["+i+"] : "+sum);
 //			i++;
+//		} 				
+		
+//		**or**
+		
+//		int sum = 0; // 총합을 저장할 변수
+//		int s=1; //값의부호를바꿔주는데사용할변수 
+//		int num = 0;
+//		
+//		// 조건식의 값이 true이므로 무한반복문이 된다.
+//		for(int i=1;true; i++, s=-s) {	 // 매 반복마다 s의 값은 1, -1, 1, -1...
+//			num = s * i; 				// i와 부호(s)를 곱해서 더할 값을 구한다. 
+//			sum += num;
+//			if(sum >=100) 				// 총합이 100보다 같거나 크면 반복문을 빠져 나간다. 
+//			break;
 //		}
+//		System.out.println("num="+num);
+//		System.out.println("sum="+sum);
 		
 //		----------------------------------------------------------------		
 //		Q4-5 다음의 for문을 while문으로 변경하시오.
@@ -245,9 +286,12 @@ public class Nam_prac {
 		
 //		----------------------------------------------------------------		
 //		Q4-6 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 프로그램을 작성하시오.
+		
+//		-> code
+		
 //		int a = (int)(Math.random()*6+1);
 //		int b = (int)(Math.random()*6+1);
-		
+//		
 //		int c = 0;
 //		for(int i = 1; i<7; i++) {
 //			for(int j = 1; j<7; j++) {
@@ -278,6 +322,7 @@ public class Nam_prac {
 //		int x, y;
 //		System.out.println("2x+4y=10 의 모든 해");
 //		System.out.println("->");
+//		
 //		for(int i = 0; i<=10; i++) { 		//x
 //			for(int j = 0; j<=10; j++) { 	//y
 //				
@@ -309,7 +354,7 @@ public class Nam_prac {
 //		
 //		for(int i =0; i<str.length(); i++) {
 //			
-//			sum = sum + (str.charAt(i)-'0');
+//			sum += (str.charAt(i)-'0');
 //			
 //			System.out.println("charAt["+i+"] : "+str.charAt(i));
 //			System.out.println("sum["+i+"] : "+sum);
@@ -326,18 +371,26 @@ public class Nam_prac {
 //		
 //		int num = 12345;
 //		int sum = 0;
-//		
-//		for(int i = 1; i <=(int)(Math.log10(num)+1); i++) {
-//			num = num%10^(i-1);
-//			System.out.println("num ["+i+"]: "+num);
-//			sum += num;
-//			System.out.println("sum : "+sum);
+		
+		
+//		for(int i = 1; i <=(int)(Math.log10(num)+1); i++) {				|
+//																		|
+//			num = num%10^(i-1);											완
+//			System.out.println("num ["+i+"]: "+num);					전
+//																		|
+//			sum += num;													틀
+//			System.out.println("sum : "+sum);							림
+//		}																|
+//																		|
+//		System.out.println("sum : "+sum);								|
+		
+		
+//		for(int i=0; num>0; i++) {
+//			sum += num%10; 	// 마지막 자리수 뽑아내기
+//			num = num/10; 	//다음 꼐산을 위해 마지막 자리수 버리기
 //		}
+//		
 //		System.out.println("sum : "+sum);
-		
-		
-		
-		
 		
 //		----------------------------------------------------------------		
 //		Q[4-11] 피보나치(Fibonnaci) 수열(數列)은 앞을 두 수를 더해서 다음 수를 만들어 나가 는 수열이다.
@@ -346,21 +399,32 @@ public class Nam_prac {
 //		1과 1부터 시작하는 피보나치수열의 10번째 수는 무엇인지 계산하는 프로그램을 완성하시오
 		
 //		-> code
-		// Fibonnaci 수열의 시작의 첫 두 숫자를 1, 1로 한다.
-		
+//		 Fibonnaci 수열의 시작의 첫 두 숫자를 1, 1로 한다.
+//		
 //		1,1,2,3,5,8,13,21,34, 55
-		//전+현 = 전
+//		전+현 = 전
+		
 //		int num1 = 1; 	// = i
 //		int num2 = 1; 	// = i
 //		
 //		
-//		int num3=num1+num2; 
-////		System.out.print(num1+","+num2);
-//
-//		for (int i = 0 ; i < 8 ; i++ ) {
+//		int num3=0; 
+//		System.out.print(num1+","+num2);
+//		
+//		int sum = 0;
+//		
+//		for(int i = 0; i<8; i++) {
+//			num3 = sum+num1+num2;
+//			
+//			System.out.print(","+ num3);
+//			//이런 방법이 있구나
+//			num1 = num2;
+//			num2 = num3;
+//		
 //			
 //		}
-//		----------------------------------------------------------------		
+		
+		//		----------------------------------------------------------------		
 //		Q4-12구구단의 일부분을 다음과 같이 출력하시오.
 		
 //		-> code
@@ -381,25 +445,25 @@ public class Nam_prac {
 		
 //		-> code
 		
-		String value = "12o34"; 
-		char ch = ' ';
-		boolean isNumber = true;
-		// 반복문과 charAt(int i)를 이용해서 문자열의 문자를 
-		// 하나씩 읽어서 검사한다.
-		
-		for(int i=0; i < value.length() ;i++) {
-		
-		if( !( (value.charAt(i)<57) && (value.charAt(i)>48) ) ) {
-			isNumber = false;
-			System.out.println(isNumber);
-		}
-		
-		} //for
-		if (isNumber) {			
-		System.out.println(value+"는 숫자입니다.");
-		} else {
-			System.out.println(value+"는 숫자가 아닙니다.");			
-		} 	//if and else
+//		String value = "12o34"; 
+//		char ch = ' ';
+//		boolean isNumber = true;
+////		// 반복문과 charAt(int i)를 이용해서 문자열의 문자를 
+////		// 하나씩 읽어서 검사한다.
+////		
+//		for(int i=0; i < value.length() ;i++) {
+//		ch=value.charAt(i);
+//		if( !( (ch<57) && (ch>48) ) ) {
+//			isNumber = false;
+//			System.out.println(isNumber);
+//		}
+//		
+//		} //for
+//		if (isNumber) {			
+//		System.out.println(value+"는 숫자입니다.");
+//		} else {
+//			System.out.println(value+"는 숫자가 아닙니다.");			
+//		} 	//if and else
 //		----------------------------------------------------------------		
 //		Q
 		
