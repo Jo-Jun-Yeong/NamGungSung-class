6563ecea23a9ee3cfaef471a365c26a7f197de85
test1 <jojun-yeong@localhost>
2021-06-21 01:22:25 +0900

    06/20 찜찜 하다

--- src/Nam_prac_5.java
+++ src/Nam_prac_5.java
@@ -120,93 +120,335 @@ public class Nam_prac_5 {
 		
 //		-> code
 		
-		if(args.length!=1) {
-			System.out.println("USAGE: java Exercise5_7 3120"); 
-			System.exit(0);
-			} 	//if
-			// 문자열을 숫자로 변환한다. 입력한 값이 숫자가 아닐 경우 예외가 발생한다. 
-		
-			int money = Integer.parseInt(args[0]);
-			
-			System.out.println("money="+money);
-			int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
-			int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수 
-			
-			for(int i=0;i<coinUnit.length;i++) {
-				int coinNum = 0;
-				
-				/* (1) 아래의 로직에 맞게 코드를 작성하시오.
-				1. 금액(money)을 동전단위로 나눠서 필요한 동전의 개수(coinNum)를 구한다. 
-				2. 배열 coin에서 coinNum만큼의 동전을 뺀다. (만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.) 
-				3. 금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
-				 */
-			
-				if(coin[i] < (money/coinUnit[i]))			//부족할 때
-				{ 	
-					System.out.println("부족");
-					
-					money = money-(coinUnit[i]*coin[i]); 		//거스름 돈
-					
-					coin[i] = coin[i]-coin[i]; 		//거스름돈의
-					coinNum = 5;	//동전의 갯수
-							
-				} 	//if
-				
-				else if(coin[i]>=(money/coinUnit[i])) 		//충분할 때
-				{ 	
-					System.out.println("충분");
-					
-					coinNum = (money/coinUnit[i] >= coin[i] ? 5 : money/coinUnit[i]); 	//동전의 갯수	
-					
-					coin[i]=  coin[i]-(money/coinUnit[i] >= coin[i] ? 5 : money/coinUnit[i]);	
-					
-					money = money-(coinUnit[i]*(money/coinUnit[i]));
-					
-					
-				
-//					System.out.println("money: "+money);
-				}	//else if
-
-				
-				
-				
-				System.out.println(coinUnit[i]+"원: "+coinNum);
-				System.out.println("money : "+money);
-//				coin[i] = coin[i];
-			} //for
-			
-			
-			if(money > 0) {
-				System.out.println("거스름돈이 부족합니다."); 
-				System.exit(0); // 프로그램을 종료한다.
-			}
-			System.out.println("=남은 동전의 개수 =");
-			for(int i=0;i<coinUnit.length;i++) { 
-				System.out.println(coinUnit[i]+"원:"+coin[i]);
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+//		if(args.length!=1) {
+//			System.out.println("USAGE: java Exercise5_7 3120"); 
+//			System.exit(0);
+//			} 	//if
+//			// 문자열을 숫자로 변환한다. 입력한 값이 숫자가 아닐 경우 예외가 발생한다. 
+//		
+//			int money = Integer.parseInt(args[0]);
+//			
+//			System.out.println("money="+money);
+//			int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
+//			int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수 
+//			
+//			for(int i=0;i<coinUnit.length;i++) {
+//				int coinNum = 0;
+//				
+//				/* (1) 아래의 로직에 맞게 코드를 작성하시오.
+//				1. 금액(money)을 동전단위로 나눠서 필요한 동전의 개수(coinNum)를 구한다. 
+//				2. 배열 coin에서 coinNum만큼의 동전을 뺀다. (만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.) 
+//				3. 금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
+//				 */
+//			
+//				if(coin[i] < (money/coinUnit[i]))			//부족할 때
+//				{ 	
+//					System.out.println("부족");
+//					
+//					money = money-(coinUnit[i]*coin[i]); 		//거스름 돈
+//					
+//					coin[i] = coin[i]-coin[i]; 		//거스름돈의
+//					coinNum = 5;	//동전의 갯수
+//							
+//				} 	//if
+//				
+//				else if(coin[i]>=(money/coinUnit[i])) 		//충분할 때
+//				{ 	
+//					System.out.println("충분");
+//					
+//					coinNum = (money/coinUnit[i] >= coin[i] ? 5 : money/coinUnit[i]); 	//동전의 갯수	
+//					
+//					coin[i]=  coin[i]-(money/coinUnit[i] >= coin[i] ? 5 : money/coinUnit[i]);	
+//					
+//					money = money-(coinUnit[i]*(money/coinUnit[i]));
+//					
+//					
+//				
+////					System.out.println("money: "+money);
+//				}	//else if
+//
+//				
+//				
+//				
+//				System.out.println(coinUnit[i]+"원: "+coinNum);
+//				System.out.println("money : "+money);
+////				coin[i] = coin[i];
+//			} //for
+//			
+//			
+//			if(money > 0) {
+//				System.out.println("거스름돈이 부족합니다."); 
+//				System.exit(0); // 프로그램을 종료한다.
+//			}
+//			System.out.println("=남은 동전의 개수 =");
+//			for(int i=0;i<coinUnit.length;i++) { 
+//				System.out.println(coinUnit[i]+"원:"+coin[i]);
+//			}		
 //		----------------------------------------------------------------		
+//		Q5-8 다음은 배열 answer에 담긴 데이터를 읽고 각 숫자의 개수를 세어서 개수만큼 
+//			‘*’ 을 찍어서 그래프를 그리는 프로그램이다.
+//			(1)~(2)에 알맞은 코드를 넣어서 완성하시오.
+			
+//			->code
+		
+//			int[] answer = { 1,4,4,3,1,4,4,2,1,3,2 };
+//			int[] counter = new int[4];
+//			
+//			for(int i=0; i < answer.length;i++) {
+//				
+//				if(answer[i] == 1) {
+//					counter[0]++;
+//				}
+//				else if (answer[i] == 2) {
+//					counter[1]++;
+//				}
+//				else if (answer[i] == 3) {
+//					counter[2]++;
+//				}
+//				else if (answer[i]==4) {
+//					counter[3]++;
+//				}
+//				
+//				
+//			/* (1) 알맞은 코드를 넣어 완성하시오. */
+//			}//for1
+//			
+//			
+//			for(int i=0; i < counter.length;i++) {
+//				System.out.print(counter[i]+" ");
+//				for(int j = 1; j<=counter[i]; j++) {
+//					System.out.print("*");
+//				}
+//				
+//			/*
+//			(2) 알맞은 코드를 넣어 완성하시오.
+//			*/
+//				System.out.println();
+//			} 	//for2
+			
+
+//		----------------------------------------------------------------		
+//		Q5-9 주어진 배열을 시계방향으로 90도 회전시켜서 출력하는 프로그램을 완성하시오.
+		
+//		->code
+		
+//			char[][] star = 
+//				{ 
+//					{'*','*',' ',' ',' '}, 
+//					{'*','*',' ',' ',' '}, 
+//					{'*','*','*','*','*'}, 
+//					{'*','*','*','*','*'}
+//				};
+//			
+//			char[][] result = new char[star[0].length][star.length];
+//			
+//			for(int i=0; i < star.length;i++) { 
+//				for(int j=0; j < star[i].length;j++) {
+//					System.out.print(star[i][j]);
+//			     }
+//			           System.out.println();
+//			}
+//			
+//			System.out.println();
+//			for(int i=0; i < star.length;i++) { 
+//				for(int j=0; j < star[i].length;j++) {
+//					
+//			/*
+//			(1) 알맞은 코드를 넣어 완성하시오.
+//			*/
+//				}
+//			}
+//	
+//			for(int i=0; i < result.length;i++) {
+//				for(int j=0; j < result[i].length;j++) {
+//					System.out.print(result[i][j]);
+//			    }
+//			        System.out.println();
+//			}
+//		----------------------------------------------------------------		
+//		Q5-10 다음은 알파벳과 숫자를 아래에 주어진 암호표로 암호화하는 프로그램이다. 
+//				(1)에 알맞은 코드를 넣어서 완성하시오.
+			
+//			-> code
+			
+//			char[] abcCode =
+//				{ 
+//					'`','~','!','@','#','$','%','^','&','*',
+//				    '(',')','-','_','+','=','|','[',']','{',
+//				    '}',';',':',',','.','/'
+//				};
+//				//0 1 2 3 4 5 6 7 8 9 
+//			
+//			char[] numCode = {'q','w','e','r','t','y','u','i','o','p'};
+//			
+//				String src = "abc123"; 
+//				String result = "";
+//				// 문자열 src의 문자를 charAt()으로 하나씩 읽어서 변환 후 result에 저장 
+//				
+//				for(int i=0; i < src.length();i++) {
+//				char ch = src.charAt(i);
+//				if(src.charAt(i)=='a') {
+//					result = result + '`';
+//				}
+//				else if(src.charAt(i)=='b') {
+//					result = result + '~';
+//				}
+//				else if(src.charAt(i)=='c') {
+//					result = result + '!';
+//				}
+//				else if(src.charAt(i)=='d') {
+//					result = result + '@';
+//				}
+//				else if(src.charAt(i)=='e') {
+//					result = result + '~';
+//				}
+//				else if(src.charAt(i)=='#') {
+//					result = result + '~';
+//				}
+//				else if(src.charAt(i)=='f') {
+//					result = result + '$';
+//				}
+//				else if(src.charAt(i)=='g') {
+//					result = result + '%';
+//				}
+//				else if(src.charAt(i)=='h') {
+//					result = result + '^';
+//				}
+//				else if(src.charAt(i)=='i') {
+//					result = result + '&';
+//				}
+//				else if(src.charAt(i)=='j') {
+//					result = result + '*';
+//				}
+//				else if(src.charAt(i)=='k') {
+//					result = result + '(';
+//				}
+//				else if(src.charAt(i)=='l') {
+//					result = result + ')';
+//				}
+//				else if(src.charAt(i)=='m') {
+//					result = result + '-';
+//				}
+//				else if(src.charAt(i)=='n') {
+//					result = result + '_';
+//				}
+//				else if(src.charAt(i)=='o') {
+//					result = result + '+';
+//				}
+//				else if(src.charAt(i)=='p') {
+//					result = result + '=';
+//				}
+//				else if(src.charAt(i)=='q') {
+//					result = result + '|';
+//				}
+//				else if(src.charAt(i)=='r') {
+//					result = result + '[';
+//				}
+//				else if(src.charAt(i)=='s') {
+//					result = result + ']';
+//				}
+//				else if(src.charAt(i)=='t') {
+//					result = result + '{';
+//				}
+//				else if(src.charAt(i)=='u') {
+//					result = result + '}';
+//				}
+//				else if(src.charAt(i)=='v') {
+//					result = result + ';';
+//				}
+//				else if(src.charAt(i)=='w') {
+//					result = result + ':';
+//				}
+//				else if(src.charAt(i)=='x') {
+//					result = result + ',';
+//				}
+//				else if(src.charAt(i)=='y') {
+//					result = result + '.';
+//				}
+//				else if(src.charAt(i)=='z') {
+//					result = result + '/';
+//				}
+//				else if(src.charAt(i)=='0') {
+//					result = result + 'q';
+//				}
+//				else if(src.charAt(i)=='1') {
+//					result = result + 'w';
+//				}
+//				else if(src.charAt(i)=='2') {
+//					result = result + 'e';
+//				}
+//				else if(src.charAt(i)=='3') {
+//					result = result + 'r';
+//				}
+//				else if(src.charAt(i)=='4') {
+//					result = result + 't';
+//				}
+//				else if(src.charAt(i)=='5') {
+//					result = result + 'y';
+//				}
+//				else if(src.charAt(i)=='6') {
+//					result = result + 'u';
+//				}
+//				else if(src.charAt(i)=='7') {
+//					result = result + 'i';
+//				}
+//				else if(src.charAt(i)=='8') {
+//					result = result + 'o';
+//				}
+//				else if(src.charAt(i)=='9') {
+//					result = result + 'p';
+//				}
+//				/*
+//				(1) 알맞은 코드를 넣어 완성하시오.
+//				*/
+//				}
+//				       System.out.println("src:"+src);
+//				       System.out.println("result:"+result);
+//		----------------------------------------------------------------		
+//		Q5-11 주어진 2차원 배열의 데이터보다 가로와 세로로 1이 더 큰 배열을 생성해서
+//			배열 의 행과 열의 마지막 요소에 각 열과 행의 총합을 저장하고 출력하는 프로그램이다.
+//			(1) 에 알맞은 코드를 넣어서 완성하시오.
+			int[][] score = {
+					{100, 100, 100}, 
+					{20, 20, 20} , 
+					{30, 30, 30} , 
+					{40, 40, 40} , 
+					{50, 50, 50}
+			};
+			
+			int[][] result = new int[score.length+1][score[0].length+1];
+			
+			for(int i=0; i < score.length;i++) { 
+				for(int j=0; j < score[i].length;j++) { //i는 최대2 j는 4
+					result[i][j] = score[i][j];
+					
+					result[i][score[i].length] += result[i][j]; 	//가로
+					
+					result[score.length][j] += result[i][j];  	//세로
+					result[score.length][score[j].length] += result[i][j];  	//세로
+					
+					
+//					result[score.length][j] = 55 ;  	//세로
+					
+					System.out.println(i);
+					//x=5고정 5 고정은 완료  y=3까지 i를 3까지 어떻게하나
+				/*
+				(1) 알맞은 코드를 넣어 완성하시오.
+				*/
+					
+				}	//for2
+				System.out.println( );
+			}	//for1
+//			System.out.println(result[0][result[0][3]]);
+			System.out.println(result.length);
+			
+			for(int i=0; i < result.length;i++) { 
+				for(int j=0; j < result[i].length;j++) {
+					System.out.printf("%4d",result[i][j]);
+				}
+				System.out.println();
+			}
+//		----------------------------------------------------		
 		
 	}		//main
 }			//class
