af5ea7dc86e3ea3766c3240a048cc26a4c4fa055
test1 <jojun-yeong@localhost>
2021-06-25 16:08:17 +0900

    06/25

--- src/prac_6_6to7.java
+++ src/prac_6_6to7.java
@@ -1,23 +1,40 @@
 import java.*;
-//두 점의 거리를 계산하는 getDistance()를 완성하시오.
+//------------------------------------------------------------
+//[6-6]두 점의 거리를 계산하는 getDistance()를 완성하시오.
 //[Hint] 제곱근 계산은 Math.sqrt(double a)를 사용하면 된다.
 //------------------------------------------------------------
-public class prac_6_6to7 {
+//[6-7] 문제6-6에서 작성한 클래스메서드 
+//getDistance()를 MyPoint클래스의 인스턴스메서드로 정의하시오.
+//------------------------------------------------------------
+class Mypoint {
+	int x; 
+	int y;
 	
-	//두점 ()와()간의 거리를 구한다
-//	Math.sqrt( (x2-x1)^2+(y2-y1)^2 )
-	static double getDistance(int x, int y, int x1, int y1) {
+	Mypoint(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	double getDistance(int x1, int y1) {
+		this.x=x;
+		this.y=y;
+		
 		double A = (x1-x)*(x1-x);
 		double B = (y1-y)*(y1-y);
+		
 		double sqrt = Math.sqrt(A+B);
 		return  sqrt;
 		
 	}
+}
+
+public class prac_6_6to7 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(getDistance(1,1,2,2));
-		System.out.println();
+		Mypoint p = new Mypoint(1,1);
+		
+		System.out.println(p.getDistance(2,2));
 		
 	} 		//main method
 }		//class 
